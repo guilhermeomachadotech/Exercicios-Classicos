b655f30d3f955bb145cab973dd1673d334a60ccd
guilhermeomachadotech <112173925+guilhermeomachadotech@users.noreply.github.com>
2022-11-17 16:09:36 -0300

    Add files via upload

---
+++ IdadePessoa.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class IdadePessoa {
+	public static void main (String[]args) {
+		Scanner ler=new Scanner(System.in);
+		int idade, idadeMaior=-1, idadeMenor=1000, i=1 ;
+		double media, somaIdades=0;
+		do {
+			System.out.println("Digite a idade do "+i+"° usuário:");
+			idade=ler.nextInt();
+			somaIdades=somaIdades+idade;
+			if(idade>idadeMaior) {
+				idadeMaior=idade;
+			}else if(idade<idadeMenor){
+				idadeMenor=idade;
+			}
+			i++;
+		}while (i<=10);
+		media=somaIdades/10;
+		System.out.println ("A média de idades é "+media);
+		System.out.println("A idade mais velha é "+idadeMaior+" anos, e a idade mais novo é "+idadeMenor+" anos");
+		ler.close();
+	}
+}
